ff646a447fcdb9d904393ded2e418bba11822646
Samuele Spina <samuele_spina@MacBook-Air-di-Samuele.local>
2025-02-18 21:41:30 +0100

    fix/macos-quit

--- src/main/main.js
+++ src/main/main.js
@@ -78,11 +78,7 @@ app.on("will-quit", () => {
     unregisterShortcuts();
 });
 
-// Quit when all windows are closed, except on macOS. There, it"s common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+// Quit when all windows are closed.
 app.on("window-all-closed", () => {
-    if (process.platform !== "darwin") {
-        app.quit();
-    }
+    app.quit();
 });
